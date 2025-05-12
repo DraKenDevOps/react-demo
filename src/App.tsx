34deabb62aecdfca67787bf4e65c1976c8c80ead
Anakin <anakin.nanthavongsa@gmail.com>
2025-05-12 15:32:08 +0700

    Edit content

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "/react.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [count, setCount] = useState(0);
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>DEMO CLOUDFLARE WORKER {new Date().getFullYear()}</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    return (
+        <>
+            <div>
+                <a href="https://react.dev" target="_blank">
+                    <img src={reactLogo} className="logo react" alt="React logo" />
+                </a>
+            </div>
+            <h1>DEMO CLOUDFLARE WORKER {new Date().getFullYear()}</h1>
+            <div className="card">
+                <button onClick={() => setCount(count => count + 1)}>COUNT IS {count}</button>
+            </div>
+        </>
+    );
 }
 
-export default App
+export default App;
